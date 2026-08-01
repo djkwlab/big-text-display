957c59aac6327d9c6c3032015f273942a4f8a8bc
djkwLab <jankowskidawid@gmail.com>
2026-08-01 11:37:14 +0200

    Fix typo in icon filename in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = 'big-text-display-v1';
-const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './iconTX.png'];
+const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './iconTx.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
