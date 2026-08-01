2144dee1f7bfcd17f3aa9753f767259cc7a2c3a0
djkwLab <jankowskidawid@gmail.com>
2026-08-01 11:58:10 +0200

    Update cache version and logo file in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'big-text-display-v3';
-const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './logo.svg'];
+const CACHE = 'big-text-display-v4';
+const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './logoTx.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
